245a6ad498b44fe93fd772cdc4356925661a0365
Hong Shuning <hongshuning@gmail.com>
2016-01-28 15:56:20 +0800

    根据行情自动刷新订单的盈亏颜色

--- static/orderlist.js
+++ static/orderlist.js
@@ -1,12 +1,58 @@
-var Order = React.createClass({
+var Price = React.createClass({
     render: function() {
         return (
-            <tr className={"order "+(this.props.order.is_long?"long":"short")}>
+            <div>当前价格: 
+                <span class="price">{this.props.curPrice}</span>
+            </div>
+        );
+    }
+});
+
+var Order = React.createClass({
+    render: function() {
+        var orderClass = "order";
+        if (this.props.order.is_long) {
+            orderClass += " long";
+        } else {
+            orderClass += " short";
+        }
+        var priceClass = "price";
+        if (this.props.order.is_long) {
+            if (this.props.curPrice > this.props.order.avg_fill_price) {
+                priceClass += " gain";
+            } else {
+                priceClass += " loss";
+            }
+        } else {
+            if (this.props.curPrice < this.props.order.avg_fill_price) {
+                priceClass += " gain";
+            } else {
+                priceClass += " loss";
+            }
+        }
+        var stopClass = "price";
+        if (this.props.order.stoploss) {
+            if (this.props.order.is_long) {
+                if (this.props.order.stoploss > this.props.order.avg_fill_price) {
+                    stopClass += " gain";
+                } else {
+                    stopClass += " loss";
+                }
+            } else {
+                if (this.props.order.stoploss < this.props.order.avg_fill_price) {
+                    stopClass += " gain";
+                } else {
+                    stopClass += " loss";
+                }
+            }
+        }
+        return (
+            <tr className={orderClass}>
                 <td className="order_time">{this.props.order.order_time}</td>
                 <td className="order_id">{this.props.order.sys_id}</td>
-                <td className="price">{this.props.order.avg_fill_price}</td>
+                <td className={priceClass}>{this.props.order.avg_fill_price}</td>
                 <td className="volume">{this.props.order.volume}</td>
-                <td className="price">{this.props.order.stoploss}</td>
+                <td className={stopClass}>{this.props.order.stoploss}</td>
                 <td className="scode">{this.props.order.scode}</td>
                 <td className="status">{this.props.order.status}</td>
             </tr>
@@ -15,33 +61,11 @@ var Order = React.createClass({
 });
 
 var OrderList = React.createClass({
-    getInitialState: function() {
-        return {data: []};
-    },
-
-    loadData: function() {
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                this.setState({data: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
-
-    componentDidMount: function() {
-        this.loadData();
-        setInterval(this.loadData, this.props.pollInterval);
-    },
-
     render: function() {
-        var orderNodes = this.state.data.map(function(order) {
+        var curPrice = this.props.curPrice;
+        var orderNodes = this.props.data.map(function(order) {
                 return (
-                    <Order order={order}></Order>
+                    <Order key={order.sys_id} order={order} curPrice={curPrice}></Order>
                 );
             });
         return (
@@ -65,7 +89,57 @@ var OrderList = React.createClass({
     }
 });
 
+var OrderListBox = React.createClass({
+    getInitialState: function() {
+        return {curPrice: "", orders: []};
+    },
+
+    loadPriceData: function() {
+        $.ajax({
+            url: this.props.priceUrl,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({curPrice: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.priceUrl, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    loadOrdersData: function() {
+        $.ajax({
+            url: this.props.ordersUrl,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({orders: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.ordersUrl, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    componentDidMount: function() {
+        this.loadPriceData();
+        this.loadOrdersData();
+        setInterval(this.loadPriceData, this.props.pricePollInterval);
+        setInterval(this.loadOrdersData, this.props.ordersPollInterval);
+    },
+
+    render: function() {
+        return (
+            <div>
+                <Price curPrice={this.state.curPrice} />
+                <OrderList data={this.state.orders} curPrice={this.state.curPrice} />
+            </div>
+        );
+    },
+});
+
 ReactDOM.render(
-    <OrderList url="/api/opened-orders/" pollInterval={10000} />,
-    document.getElementById('order_list')
+    <OrderListBox priceUrl="/api/current-price/" pricePollInterval={1000} ordersUrl="/api/opened-orders/" ordersPollInterval={10000} />,
+    document.getElementById('content')
 );
